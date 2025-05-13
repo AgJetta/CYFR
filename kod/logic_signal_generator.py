afa11dda98a0c069c50192c371c9df784073a999
a-vishniavetski <a.vishniavetski@gmail.com>
2025-05-13 15:12:01 +0200

    FIX: time in generating signals; feat: parameter range adjustment; PERIOD is now denoting frequency (awkward naming, change later);

--- kod/logic_signal_generator.py
+++ kod/logic_signal_generator.py
@@ -5,40 +5,49 @@ from strings import (MEAN_VALUE, ABSOLUTE_MEAN_VALUE, RMS_VALUE,
 
 class SignalGenerator:
     @staticmethod
+    def get_timeseries(start_time, duration, sampling_rate):
+        t = np.linspace(start_time, start_time + duration, int(duration * sampling_rate) + 1)
+        return t
+    
+    @staticmethod
     def generate_uniform_noise(amplitude, start_time, duration, sampling_rate=1000):
-        t = np.linspace(start_time, start_time + duration, int(duration * sampling_rate))
+        t = SignalGenerator.get_timeseries(start_time, duration, sampling_rate)
         noise = np.random.uniform(-amplitude, amplitude, len(t))  # U(-A, A)
         return t, noise
 
+
     @staticmethod
     def generate_gaussian_noise(amplitude, start_time, duration, sampling_rate=1000):
-        t = np.linspace(start_time, start_time + duration, int(duration * sampling_rate))
+        t = SignalGenerator.get_timeseries(start_time, duration, sampling_rate)
         noise = amplitude * np.random.normal(0, 1, len(t))  # u = 0, sigma = 1
         return t, noise
 
     @staticmethod
-    def generate_sinusoidal(amplitude, period, start_time, duration, sampling_rate=1000):
-        t = np.linspace(start_time, start_time + duration, int(duration * sampling_rate))
-        signal = amplitude * np.sin(2 * np.pi * (t - start_time) / period)
+    def generate_sinusoidal(amplitude, signal_frequency, start_time, duration, sampling_rate=1000):
+        t = SignalGenerator.get_timeseries(start_time, duration, sampling_rate)
+        signal = amplitude * np.sin(2 * np.pi * (t - start_time) * signal_frequency)
+
+        print(f"Time: {t}\n")
+        print(f"Signal: {signal}\n")
         return t, signal
 
     @staticmethod
     def generate_half_wave_rectified(amplitude, period, start_time, duration, sampling_rate=1000):
-        t = np.linspace(start_time, start_time + duration, int(duration * sampling_rate))
+        t = SignalGenerator.get_timeseries(start_time, duration, sampling_rate)
         sin_wave = np.sin(2 * np.pi * (t - start_time) / period)
         signal = amplitude / 2 * (sin_wave + np.abs(sin_wave))
         return t, signal
 
     @staticmethod
     def generate_full_wave_rectified(amplitude, period, start_time, duration, sampling_rate=1000):
-        t = np.linspace(start_time, start_time + duration, int(duration * sampling_rate))
+        t = SignalGenerator.get_timeseries(start_time, duration, sampling_rate)
         sin_wave = np.sin(2 * np.pi * (t - start_time) / period)
         signal = amplitude * np.abs(sin_wave)
         return t, signal
 
     @staticmethod
     def generate_square_wave(amplitude, period, start_time, duration, duty_cycle=0.5, sampling_rate=1000):
-        t = np.linspace(start_time, start_time + duration, int(duration * sampling_rate))
+        t = SignalGenerator.get_timeseries(start_time, duration, sampling_rate)
         signal = amplitude * (((t - start_time) % period) / period <= duty_cycle).astype(float)
         return t, signal
 
@@ -50,14 +59,14 @@ class SignalGenerator:
 
     @staticmethod
     def generate_triangular_wave(amplitude, period, start_time, duration, duty_cycle=0.5, sampling_rate=1000):
-        t = np.linspace(start_time, start_time + duration, int(duration * sampling_rate))
+        t = SignalGenerator.get_timeseries(start_time, duration, sampling_rate)
         phase = (t - start_time) % period / period
         signal = amplitude * (2 * np.abs(2 * (phase - duty_cycle)) - 1)
         return t, signal
 
     @staticmethod
     def generate_unit_step(amplitude, start_time, duration, step_time=None, sampling_rate=1000):
-        t = np.linspace(start_time, start_time + duration, int(duration * sampling_rate))
+        t = SignalGenerator.get_timeseries(start_time, duration, sampling_rate)
         signal = np.zeros_like(t)
         mask = t > step_time
         signal[mask] = amplitude
@@ -75,7 +84,7 @@ class SignalGenerator:
 
     @staticmethod
     def generate_unit_noise(amplitude, start_time, duration, sampling_rate=1000, probability=0.5):
-        t = np.linspace(start_time, start_time + duration, int(duration * sampling_rate))
+        t = SignalGenerator.get_timeseries(start_time, duration, sampling_rate)
         signal = amplitude * np.random.binomial(1, probability, len(t))
         return t, signal
 
